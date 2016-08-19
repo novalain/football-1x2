f52059dfcd4db7294a4e43ea7fa2620972761c1b
denFete <halling.7@hotmail.com>
2016-08-19 18:55:26 +0200

    Startscreen design

--- www/js/controllers/game_controller.js
+++ www/js/controllers/game_controller.js
@@ -1,15 +1,29 @@
 'use strict';
 angular.module('liveScoreUpdaterApp')
-.controller('gameController', function($scope, fetchService) {
+.controller('gameController', function($scope, fetchService, $timeout) {
 	$scope.detailedView = true;
-
   fetchService.fetchGame('stryktipset').then(data => {
     $scope.games = data.games;
+    watchGameStarted();
     $scope.$apply();
   });
   $scope.week = new Date().getWeekNumber();
 
-  $scope.toggleViewChange = function() {
+ 	$scope.toggleViewChange = function() {
   	$scope.detailedView = !$scope.detailedView;
   }
+
+  function watchGameStarted() {
+  	const currentTime = Date.now();
+  	$scope.games.forEach(game => {
+  		const gameDate = new Date(game.date);
+  		if (gameDate.getTime() < currentTime) {
+  			game.hasStarted = true;
+  		}
+  	});
+  	$timeout(function() {
+  		watchGameStarted();
+  	},5000)
+  }
+
 });
