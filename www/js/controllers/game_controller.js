67e3baa44043806dfa105c003501dbc2c02fd482
denFete <halling.7@hotmail.com>
2016-08-19 11:28:15 +0200

    Button done

--- www/js/controllers/game_controller.js
+++ www/js/controllers/game_controller.js
@@ -1,9 +1,15 @@
 'use strict';
 angular.module('liveScoreUpdaterApp')
 .controller('gameController', function($scope, fetchService) {
+	$scope.detailedView = true;
+
   fetchService.fetchGame('stryktipset').then(data => {
     $scope.games = data.games;
     $scope.$apply();
   });
   $scope.week = new Date().getWeekNumber();
+
+  $scope.toggleViewChange = function() {
+  	$scope.detailedView = !$scope.detailedView;
+  }
 });
